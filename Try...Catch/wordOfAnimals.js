78f94cae55e1e5c21b8d498b26ecb24ae7df7894
Alex Schmidt <sa-sch12@mail.ru>
2023-02-10 00:30:14 +0300

    Word Of Animals

--- Try...Catch/wordOfAnimals.js
+++ Try...Catch/wordOfAnimals.js
@@ -62,50 +62,92 @@
 // Я всегда могу придумать функцию, которая будет ПРИНИМАТЬ аргументы и ВОЗВРАЩАТЬ значение
 
 
-// TODO существа имеют разный размер популяции
-// TODO выводиться в консоль должны иконки а не объекты
-// TODO говорит о том, может ли животное съесть другое животное - метод животного
-// TODO подумать нет ли кода, который выполняет одно и то же но пишется в нескольких местах (дублируется)
+// TODO существа имеют разный размер популяции √
+// TODO выводиться в консоль должны иконки а не объекты √
+// TODO говорит о том, может ли животное съесть другое животное - метод животного √
+// TODO подумать нет ли кода, который выполняет одно и то же но пишется в нескольких местах (дублируется) √
 
 class Nature {
     constructor() {
         this.wildNature = []
     }
     pushThemUp() {
-        console.log(numberOfIndividuals)
-        for (let i = 0; i < numberOfIndividuals; i++) {
+        let populationOfPredators = this.generateRandomNumber(1, 5)
+        let populationOfHerbivores = this.generateRandomNumber(5, 15)
+        let populationOfGrasses = this.generateRandomNumber(10, 20)
+
+        // this.wildNature = [
+        //     ...Array.from(Array(populationOfPredators), (_, i) => new Predator(i)),
+        //     ...Array.from(Array(populationOfHerbivores), (_, i) => new Herbivore(i)),
+        //     ...Array.from(Array(populationOfGrasses), (_, i) => new Grass(i)),
+        // ];
+
+        for (let i = 0; i < populationOfPredators; i++) {
             this.wildNature.push(new Predator(i))
-            this.wildNature.push(new Grass(i))
+        }
+        for (let i = 0; i < populationOfHerbivores; i++) {
             this.wildNature.push(new Herbivore(i))
         }
-        console.log(numberOfIndividuals)
-        this.sort()
-        this.showResult()
-        this.fight()
-    }
-    population() {
+        for (let i = 0; i < populationOfGrasses; i++) {
+            this.wildNature.push(new Grass(i))
+        }
 
+        this.sort()
+        this.statistics()
+        this.fight()
+        this.statistics()
+        this.showResult()
+    }
+    generateRandomNumber(minNumber, maxNumber) {
+        return Math.floor(Math.random() * (maxNumber - minNumber) + minNumber)
+    }
+    statistics() {
+        let howManyGrasses = this.wildNature.filter(item => item instanceof Grass).length
+        let howManyPredators = this.wildNature.filter(item => item instanceof Predator).length
+        let howManyHerbivores = this.wildNature.filter(item => item instanceof Herbivore).length
+        console.log(`травы : ${howManyGrasses} штук`)
+        console.log(`хищников : ${howManyPredators} штук`)
+        console.log(`травоядных : ${howManyHerbivores} штук`)
+
+        let natureView = this.wildNature.map((creature) => creature.name)
+        console.log(natureView)
     }
     fight() {
+        // let start = this.wildNature.length
+        // for (let i = 0; i < this.wildNature.length; i++) {
+        //     if (this.wildNature[i] instanceof Predator && i < this.wildNature.length - 1 && this.wildNature[i + 1] instanceof Herbivore) {
+        //         this.wildNature.splice(i + 1, 1)
+        //     }
+        //     if (this.wildNature[i] instanceof Predator && i > 0 && this.wildNature[i - 1] instanceof Herbivore) {
+        //         this.wildNature.splice(i - 1, 1)
+        //     }
+        //     if (this.wildNature[i] instanceof Herbivore && i < this.wildNature.length - 1 && this.wildNature[i + 1] instanceof Grass) {
+        //         this.wildNature.splice(i + 1, 1)
+        //     }
+        //     if (this.wildNature[i] instanceof Herbivore && i > 0 && this.wildNature[i - 1] instanceof Grass) {
+        //         this.wildNature.splice(i - 1, 1)
+        //     }
+        // }
+        // if (start > this.wildNature.length) {
+        //     this.fight()
+        // }
+
         let start = this.wildNature.length
         for (let i = 0; i < this.wildNature.length; i++) {
-            if (this.wildNature[i] instanceof Predator && i < this.wildNature.length - 1 && this.wildNature[i + 1] instanceof Herbivore) {
+            if (i < this.wildNature.length - 1 && this.wildNature[i].canEat(this.wildNature[i + 1])) {
                 this.wildNature.splice(i + 1, 1)
             }
-            if (this.wildNature[i] instanceof Predator && i > 0 && this.wildNature[i - 1] instanceof Herbivore) {
+            if (i > 0 && this.wildNature[i].canEat(this.wildNature[i - 1])) {
                 this.wildNature.splice(i - 1, 1)
             }
-            if (this.wildNature[i] instanceof Herbivore && i < this.wildNature.length - 1 && this.wildNature[i + 1] instanceof Grass) {
-                this.wildNature.splice(i + 1, 1)
-            }
-            if (this.wildNature[i] instanceof Herbivore && i > 0 && this.wildNature[i - 1] instanceof Grass) {
-                this.wildNature.splice(i - 1, 1)
-            }
+            // Обьяв. переменная , условие , шаг , тело цикла
+            // Каждый шаг выполняется тело цикла 
+            // Цикл заканчивается когда условие выдаст фалсе
+            // В ифе проверяется находится ли животное сосед за пределами массива, проверяет может ли его сьесть
         }
+
         if (start > this.wildNature.length) {
             this.fight()
-        } else {
-            this.showResult()
         }
     }
     sort() {
@@ -115,10 +157,6 @@ class Nature {
         let howManyGrasses = this.wildNature.filter(item => item instanceof Grass).length
         let howManyPredators = this.wildNature.filter(item => item instanceof Predator).length
         let howManyHerbivores = this.wildNature.filter(item => item instanceof Herbivore).length
-        console.log(`травы : ${howManyGrasses} штук`)
-        console.log(`хищников : ${howManyPredators} штук`)
-        console.log(`травоядных : ${howManyHerbivores} штук`)
-        console.log(nature.wildNature)
         if (howManyPredators > howManyGrasses && howManyPredators > howManyHerbivores) {
             console.log(`победили хищники`)
         }
@@ -139,6 +177,9 @@ class Grass {
     get name() {
         return this._name
     }
+    canEat() {
+        return false
+    }
 }
 class Herbivore {
     constructor(num) {
@@ -148,7 +189,13 @@ class Herbivore {
     get name() {
         return this._name
     }
-
+    canEat(canIeat) {
+        if (canIeat instanceof Grass) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
 class Predator {
     constructor(num) {
@@ -158,10 +205,17 @@ class Predator {
     get name() {
         return this._name
     }
-
+    canEat(canIeat) {
+        // принимает аргумент элемента соседа по массиву 
+        // проверяет его на наследство от конкретного класса
+        // возвращает тру или фалсе
+        if (canIeat instanceof Herbivore) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
-
-
 const nature = new Nature()
 
 nature.pushThemUp()
