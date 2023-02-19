a462a8adc3e1493d54e34320398f62c08377c653
Alex Schmidt <sa-sch12@mail.ru>
2023-02-19 17:10:43 +0300

    Debt function

--- Promises/debt.js
+++ Promises/debt.js
@@ -11,63 +11,51 @@
 // но после этого  он теряет ПОЛОВИНУ своих денег и ВСЕ деньги что он взял в долг
 // Когда число дней истечет Sanya должен ПРОВЕРИТЬ хватит ли у него СВОИХ денег чтобы отдать долг
 
+// class Sanya {
+//     constructor(money, days) {
+//         this.money = money
+//         this.days = days
+//     }
+//     then(onfulfilled, onrejected) {
+//         const salary = 8 * this.days
+//         const sanyasMoney = salary / 2
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                 if (sanyasMoney >= this.money) {
+//                     resolve(onfulfilled(this.money))
+//                 } else {
+//                     const error = new Error('у санька закончились деньги');
+//                     if (onrejected) {
+//                         onrejected(error);
+//                     } else {
+//                         reject(error)
+//                     }
+//                 }
+//             }, this.days * 1000)
+//         })
+//     }
+// }
 
-
-class Sanya {
-    constructor(money, days) {
-        this._money = money
-        this._days = days
-        this._sanyasMoney = 0
-    }
-    get sanyasMoney() {
-        return this._sanyasMoney
-    }
-    get money() {
-        return this._money
-    }
-    get days() {
-        return this._days
-    }
-    then(resolve) {
-        let salaryForTheDay = 8 * this._days
-        return new Promise((_, reject) => {
-            setTimeout(() => {
-                this._sanyasMoney += salaryForTheDay / 2
-                if (this._sanyasMoney >= this._money) {
-                    resolve(this._money)
-                }
-                else {
-                    reject(new Error('у санька закончились деньги'))
-                }
-            }, this._days * 1000)
-        })
-    }
+function debt(money, days) {
+    const salary = 8 * days
+    const sanyasMoney = salary / 2
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (sanyasMoney >= money) {
+                resolve(money)
+            } else {
+                reject(new Error('у санька закончились деньги'))
+            }
+        }, days * 1000)
+    })
 }
 
-const sanya = new Sanya(10, 1);
+// const sanya = new Sanya(10, 1);
+const sanya = debt(10, 3)
 sanya
-    .then((money) => console.log(`Санёк вернул ${money} тысяч рублей`))
+    .then(
+        (money) => console.log(`Санёк вернул ${money} тысяч рублей`),
+        // (error) => console.log("Санёк не вернул бабки, потому что:", error.message)
+    )
+    .then((value) => console.log('here', value))
     .catch((error) => console.log("Санёк не вернул бабки, потому что:", error.message))
-
-// function debt(money, days) {
-//     let debtMoney = money
-//     let sanyasMoney = 0
-//     return new Promise((resolve, reject) => {
-//         setTimeout(function day() {
-//             sanyasMoney += 8 - 4
-//             if (sanyasMoney < 0) {
-//                 reject(new Error('eбобо'))
-//             } else if (sanyasMoney >= money) {
-//                 resolve(money)
-//             } else {
-//                 console.log('в следующий день верну')
-//                 setTimeout(day, days * 1000)
-//             }
-//         }, days * 1000)
-
-//     })
-// }
-// let promise = debt(10, 1)
-// promise
-//     .then((money) => console.log(`Санёк вернул ${money} тысяч рублей`))
-//     .catch((error) => console.log(`Санёк не вернул бабки, потому что: `, error.message))
