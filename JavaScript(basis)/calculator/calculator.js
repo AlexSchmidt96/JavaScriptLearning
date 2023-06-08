92f1dcb3f75ebca4b525220d0ca5d21b478d4b45
Alex Schmidt <sa-sch12@mail.ru>
2023-06-08 21:23:18 +0300

    deleted % button, added del button

--- JavaScript(basis)/calculator/calculator.js
+++ JavaScript(basis)/calculator/calculator.js
@@ -11,6 +11,7 @@
 function calculator() {
     const calculator = document.querySelector('.calc')
     const display = document.querySelector('.calculator-display')
+    const buttons = document.querySelector('.buttons')
     buttons.addEventListener('click', function (event) {
         const target = event.target
         if (target.tagName === 'BUTTON') {
@@ -18,24 +19,11 @@ function calculator() {
             const content = target.innerHTML
             const displayOutput = display.innerHTML
             const previousKeyType = calculator.dataset.previousKeyType
-            switch (action) {
-                case 'percent':
-                case 'divide':
-                case 'multiply':
-                case 'subtract':
-                case 'add':
-                    calculator.dataset.previousKeyType = 'operator'
-                    calculator.dataset.firstValue = displayOutput
-                    calculator.dataset.operator = action
-            }
+            const firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            const secondValue = displayOutput
             if (action === 'decimal') {
                 display.innerHTML = displayOutput + '.'
-            }
-            if (action === 'calculate') {
-                const firstValue = calculator.dataset.firstValue
-                const operator = calculator.dataset.operator
-                const secondValue = displayOutput
-                display.innerHTML = calculate(firstValue, secondValue, operator)
             }
             if (!action) {
                 if (displayOutput === '0' || previousKeyType === 'operator') {
@@ -45,6 +33,30 @@ function calculator() {
                     display.innerHTML = displayOutput + content
                 }
             }
+            switch (action) {
+                case 'percent':
+                case 'divide':
+                case 'multiply':
+                case 'subtract':
+                case 'add':
+                    calculator.dataset.previousKeyType = 'operator'
+                    calculator.dataset.firstValue = displayOutput
+                    calculator.dataset.operator = action
+                    display.innerHTML = target.innerHTML
+            }
+
+            if (action === 'calculate') {
+                display.innerHTML = calculate(firstValue, secondValue, operator)
+            }
+
+            if (action === 'del') {
+                display.innerHTML = displayOutput.substring(0, displayOutput.length - 1);
+            }
+
+            if (action === 'clear') {
+                display.innerHTML = '0'
+            }
+
             function calculate(num1, num2, operator) {
                 let result = ''
                 if (operator === 'add') {
@@ -52,7 +64,7 @@ function calculator() {
                 } else if (operator === 'subtract') {
                     result = parseFloat(num1) - parseFloat(num2)
                 } else if (operator === 'percent') {
-                    result = parseFloat(num1) % parseFloat(num2)
+                    result = parseFloat(num1) / 100
                 } else if (operator === 'multiply') {
                     result = parseFloat(num1) * parseFloat(num2)
                 } else if (operator === 'divide') {
@@ -61,6 +73,7 @@ function calculator() {
 
                 return result
             }
+
         }
     })
 }
